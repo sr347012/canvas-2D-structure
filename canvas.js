b301ef2501d7c0de667e2cda32585dcb0cf08af8
Sriramachandra <sriramachandra2387.k@gmail.com>
2022-02-13 11:52:08 +0530

    basic Elements

---
+++ canvas.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById('canvas');
+ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.fillRect(100,100,100,100)
